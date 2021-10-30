d40d2e975bfe337792c9a5e1673754b579fb225c
Topher Winward <github_82138@topherwinward.com>
2021-10-30 18:05:15 +0100

    Update ProductPage.tsx

--- components/products/ProductPage.tsx
+++ components/products/ProductPage.tsx
@@ -1,6 +1,42 @@
+import React, { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
 import { Product } from "../../products";
 import ProductCard from "./ProductCard";
 
 export default function ProductPage({ product }: { product: Product }) {
-  return <ProductCard product={product} />;
+  return (
+    <div>
+      <ProductCard product={product} />
+      <BrowserOnly>
+        <ExampleInformation />
+      </BrowserOnly>
+    </div>
+  );
+}
+
+function ExampleInformation() {
+  const cookies = new Cookies().getAll();
+  const auth = cookies["Authorization"];
+
+  return (
+    <div>
+      <div>
+        <span>Auth?</span>
+        <span>{auth ? auth : "None"}</span>
+      </div>
+    </div>
+  );
+}
+
+function BrowserOnly({ children }: { children: React.ReactNode }) {
+  const [enabled, setEnabled] = useState(false);
+  useEffect(() => {
+    setEnabled(true);
+  }, []);
+
+  if (enabled) {
+    return <>{children}</>;
+  } else {
+    return <></>;
+  }
 }
