52344e1dacc0d8dd07caef62267eedaba392f0d1
Topher Winward <github_82138@topherwinward.com>
2021-10-31 15:16:03 +0000

    Update ProductPage.tsx

--- components/products/ProductPage.tsx
+++ components/products/ProductPage.tsx
@@ -44,7 +44,7 @@ export default function ProductPage({
 
   return (
     <div className="w-full flex justify-center">
-      <div className="max-w-3xl space-y-8 mt-8">
+      <div className="max-w-3xl space-y-8 my-8">
         <div className="w-full flex justify-center">
           <div className="max-w-4xl">
             {showFallback ? (
