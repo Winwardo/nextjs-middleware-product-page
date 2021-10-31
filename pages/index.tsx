c7e8d10f0d2152b2275c075602b53a20225cb6dd
Topher Winward <github_82138@topherwinward.com>
2021-10-31 13:45:56 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,4 @@
-import { Fragment } from "react";
-import Link from "next/link";
-
-import { allProducts, Product } from "../products";
+import { AllProducts } from "../components/products/ProductPage";
 export default function Page() {
-  // In a real scenario you wouldn't ship the private products to the client, but this page is an example to avoid unnecessary complexity.
-  const publicProducts = allProducts.filter(
-    (product) => product.private === false
-  );
-  const privateProducts = allProducts.filter(
-    (product) => product.private === true
-  );
-
-  return (
-    <div>
-      <h1>All products</h1>
-      <section>
-        <h2>Public products</h2>
-        <ProductList products={publicProducts} />
-      </section>
-      <section>
-        <h2>Private products</h2>
-        <ProductList products={privateProducts} />
-      </section>
-    </div>
-  );
-}
-
-function ProductList({ products }: { products: Array<Product> }) {
-  return (
-    <ul>
-      {products.map((product) => {
-        return (
-          <Fragment key={product.id}>
-            <li>
-              <Link href={`product/${product.id}`}>{product.name}</Link>
-            </li>
-          </Fragment>
-        );
-      })}
-    </ul>
-  );
+  return <AllProducts />;
 }
