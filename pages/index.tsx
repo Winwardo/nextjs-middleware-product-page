254f3048cbb40cb0d68668b148219e199397fc26
Topher Winward <github_82138@topherwinward.com>
2021-11-01 19:12:26 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ export default function Page() {
   return (
     <div className="w-full flex justify-center">
       <div className="max-w-3xl space-y-8 my-8">
-        <div className="w-full flex justify-center">
+        <div className="w-full flex flex-col items-center justify-center">
           <AllProducts />
           <Explanation />
         </div>
