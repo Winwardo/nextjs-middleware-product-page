e710ece0a3a7a6072236cf02ab7c6d92cc0b42a8
Topher Winward <github_82138@topherwinward.com>
2021-11-01 19:06:54 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,13 @@
-import { AllProducts } from "../components/products/ProductPage";
+import { AllProducts, Explanation } from "../components/products/ProductPage";
 export default function Page() {
-  return <AllProducts />;
+  return (
+    <div className="w-full flex justify-center">
+      <div className="max-w-3xl space-y-8 my-8">
+        <div className="w-full flex justify-center">
+          <AllProducts />
+          <Explanation />
+        </div>
+      </div>
+    </div>
+  );
 }
