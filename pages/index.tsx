608449afb4b2b5cd73f66fa6c0dfa09c00a012e4
Topher Winward <github_82138@topherwinward.com>
2021-11-01 19:18:00 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import { AllProducts, Explanation } from "../components/products/ProductPage";
 export default function Page() {
   return (
     <div className="w-full flex justify-center">
-      <div className="max-w-3xl space-y-8 my-8">
-        <div className="w-full flex flex-col items-center justify-center">
+      <div className="max-w-3xl my-8">
+        <div className="w-full flex flex-col items-center gap-y-8 justify-center">
           <AllProducts />
           <Explanation />
         </div>
